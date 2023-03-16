6d9c7bbe74afe3679ac0e594ba7c134488010a44
Гераськин Константин <kostyakostikov2006@gmail.com>
2023-03-16 19:20:45 +0300

    ss

---
+++ FirstPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Pract1.newDataSetTableAdapters;
+using System.Windows.Controls;
+
+namespace Pract1
+{
+    
+    public partial class FirstPage : Page
+    {
+        pTableAdapter post1 = new pTableAdapter();
+
+        public FirstPage()
+        {
+            InitializeComponent();
+            post.ItemsSource = post1.GetData();
+        }
+    }
+}
