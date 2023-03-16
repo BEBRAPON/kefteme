87055b592a77105ce1c2dfffc8fd408521457172
Гераськин Константин <kostyakostikov2006@gmail.com>
2023-03-16 19:20:45 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using Pract1.newDataSetTableAdapters;
+
+namespace Pract1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void per_Click(object sender, RoutedEventArgs e)
+        {
+            PAGE.Content = new FirstPage();
+        }
+
+        private void per2_Click(object sender, RoutedEventArgs e)
+        {
+            PAGE.Content = new SecondPage();
+        }
+    }
+}
---
+++ SecondPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+using Pract1.newDataSetTableAdapters;
+
+namespace Pract1
+{
+
+    public partial class SecondPage : Page
+    {
+        sTableAdapter staff1 = new sTableAdapter();
+        public SecondPage()
+        {
+            InitializeComponent();
+            staff.ItemsSource = staff1.GetData();
+        }
+
+    }
+}
